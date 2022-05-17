f3dbd137ff06256fa03f773a993d784aca538287
TheodorPascu <pascutheodor22@gmail.com>
2022-05-18 00:33:39 +0300

    adauga seminar pana la 10,11

---
+++ Seminar10/src/state/main/Main.java
@@ -0,0 +1,17 @@
+package state.main;
+
+
+import state.clase.Autobuz;
+
+public class Main {
+    public static void main(String[] args) {
+        Autobuz autobuz = new Autobuz(129);
+
+        autobuz.ajungeLaCapat();
+        autobuz.ieseDinService();
+        autobuz.pleacaInCursa();
+        autobuz.ajungeLaCapat();
+        autobuz.intraInService();
+        autobuz.ieseDinService();
+    }
+}
